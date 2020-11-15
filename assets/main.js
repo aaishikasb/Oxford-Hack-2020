df2b15d1bb96c91003b82b5528cf33ffe2457379
aaishikasb <mail.aaishika@gmail.com>
2020-11-15 22:37:32 +0530

    Add Subpages

--- assets/main.js
+++ assets/main.js
@@ -1,80 +1,105 @@
 // Initialize Firebase (ADD YOUR OWN DATA)
 var config = {
-    /*apiKey: "xxxxx",
-    authDomain: "xxxxx",
-    databaseURL: "xxxxx",
-    projectId: "xxxxx",
-    storageBucket: "xxxxx",
-    messagingSenderId: "xxxxx" */
-      apiKey: "AIzaSyABEmaxV5UvgPOkLmXdTymFUUi_79hk0Aw",
-      authDomain: "sendcodes-8dd6a.firebaseapp.com",
-      databaseURL: "https://sendcodes-8dd6a.firebaseio.com",
-      projectId: "sendcodes-8dd6a",
-      storageBucket: "sendcodes-8dd6a.appspot.com",
-      messagingSenderId: "601989680069",
-      appId: "1:601989680069:web:47623f05ca505a2ce699ac"
-  };
-  firebase.initializeApp(config);
-  
-  // Reference messages collection
-  var messagesRef = firebase.database().ref('messages');
-  
-  // Listen for form submit
-  document.getElementById('contactForm').addEventListener('submit', submitForm);
-  
-  // Submit form
-  function submitForm(e){
-    e.preventDefault();
-  
-    // Get values
-    var name = getInputVal('name');
-    var age = getInputVal('age');
-    var height = getInputVal('height');
-    var phone = getInputVal('phone');
-    var complexion = getInputVal('complexion');
-    var hair = getInputVal('hair');
-    var eyes = getInputVal('eyes');
-    var birthmarks = getInputVal('birthmarks');
-    var gender = getInputVal('gender');
-    var last_seen = getInputVal('last_seen');
-    var build = getInputVal('build');
-    var outfit = getInputVal('outfit');
-  
-    // Save message
-    saveMessage(name, age, height, phone, complexion, hair, eyes, birthmarks, gender, last_seen, build, outfit);
-  
-    // Show alert
-    document.querySelector('.alert').style.display = 'block';
-  
-    // Hide alert after 3 seconds
-    setTimeout(function(){
-      document.querySelector('.alert').style.display = 'none';
-    },3000);
-  
-    // Clear form
-    document.getElementById('contactForm').reset();
+  /*apiKey: "xxxxx",
+  authDomain: "xxxxx",
+  databaseURL: "xxxxx",
+  projectId: "xxxxx",
+  storageBucket: "xxxxx",
+  messagingSenderId: "xxxxx" */
+    apiKey: "AIzaSyABEmaxV5UvgPOkLmXdTymFUUi_79hk0Aw",
+    authDomain: "sendcodes-8dd6a.firebaseapp.com",
+    databaseURL: "https://sendcodes-8dd6a.firebaseio.com",
+    projectId: "sendcodes-8dd6a",
+    storageBucket: "sendcodes-8dd6a.appspot.com",
+    messagingSenderId: "601989680069",
+    appId: "1:601989680069:web:47623f05ca505a2ce699ac"
+};
+firebase.initializeApp(config);
+console.log(firebase)
+
+function uploadImage(){
+  const ref = firebase.storage().ref();
+
+  const file = document.querySelector("#photo").files[0];
+
+  const name = new Date() + '-' + file.name
+
+  const metadata = {
+    contentType:file.type
   }
-  
-  // Function to get get form values
-  function getInputVal(id){
-    return document.getElementById(id).value;
-  }
-  
-  // Save message to firebase
-  function saveMessage(name, age, height, phone, complexion, hair, eyes, birthmarks, gender, last_seen, build, outfit){
-    var newMessageRef = messagesRef.push();
-    newMessageRef.set({
-      name: name,
-      age:age,
-      height:height,
-      phone:phone,
-      complexion:complexion,
-      hair:hair,
-      eyes:eyes,
-      birthmarks:birthmarks,
-      gender:gender,
-      last_seen:last_seen,
-      build:build,
-      outfit:outfit
-    });
-  }
+
+  const task = ref.child(name).put(file,metadata)
+
+  task
+  .then(snapshot => snapshot.ref.getDownloadURL())
+  .then(url => {
+    console.log(url)
+    alert("Image Upload Successful")
+    const image = document.querySelector('#image')
+    image.src = url
+  })
+
+}
+
+// Reference messages collectionzzz
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  // Get values
+  var name = getInputVal('name');
+  var age = getInputVal('age');
+  var height = getInputVal('height');
+  var phone = getInputVal('phone');
+  var complexion = getInputVal('complexion');
+  var hair = getInputVal('hair');
+  var eyes = getInputVal('eyes');
+  var birthmarks = getInputVal('birthmarks');
+  var gender = getInputVal('gender');
+  var last_seen = getInputVal('last_seen');
+  var build = getInputVal('build');
+  var outfit = getInputVal('outfit');
+
+  // Save message
+  saveMessage(name, age, height, phone, complexion, hair, eyes, birthmarks, gender, last_seen, build, outfit);
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, age, height, phone, complexion, hair, eyes, birthmarks, gender, last_seen, build, outfit){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    age:age,
+    height:height,
+    phone:phone,
+    complexion:complexion,
+    hair:hair,
+    eyes:eyes,
+    birthmarks:birthmarks,
+    gender:gender,
+    last_seen:last_seen,
+    build:build,
+    outfit:outfit
+  });
+}
